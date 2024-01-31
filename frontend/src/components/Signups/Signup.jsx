a694bd24176a98208fa2884f3d781c168a926b2e
nikkamendoza <123077713+nikkamendoza@users.noreply.github.com>
2024-01-31 18:28:38 +0800

    signup page
    end time: 1:16.33

--- frontend/src/components/Signups/Signup.jsx
+++ frontend/src/components/Signups/Signup.jsx
@@ -119,6 +119,20 @@ const Signup = () => {
                     <RxAvatar className="h-8 w-8" />
                   )}
                 </span>
+                <label
+                  htmlFor="file-input"
+                  className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg bg-white hover:bg-gray-50"
+                >
+                  <span>Upload a file</span>
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="file-input"
+                    accept=".jpg, .jpeg, .png"
+                    onChange={handleFileInputChange}
+                    className="sr-only"
+                  />
+                </label>
               </div>
             </div>
 
@@ -131,9 +145,9 @@ const Signup = () => {
               </button>
             </div>
             <div className={`${style.normalFlex} w-full`}>
-              <h4>Not have any account?</h4>
-              <Link to="/sign-up" className="text-blue-600 pl-2">
-                Sign Up
+              <h4>Already have an account?</h4>
+              <Link to="/login" className="text-blue-600 pl-2">
+                Login
               </Link>
             </div>
           </form>
