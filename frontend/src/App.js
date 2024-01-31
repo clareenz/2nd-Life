d4f97acbe0c9dba507179e8f4dd35ff482ed8c5f
howtodoitpleasehelp <smacale@up.edu.ph>
2024-01-31 11:58:41 +0800

    Until login page
    Buggy. hindi alligned ang forgot password

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./App.css";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {LoginPage} from "./Routes.js";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}> </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from "../components/Logins/Login.jsx";
+
+const LoginPage = () => {
+  return (
+    <div>
+        <LoginPage/>
+    </div>
+  )
+}
+
+export default Login
