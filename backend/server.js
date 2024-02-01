1d792bd6954b225d12e71f716ad894e13fe7ee53
clareenz <148300455+clareenz@users.noreply.github.com>
2024-02-01 11:49:54 +0800

    may mali

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ connectDatabase();
 // create server
 const server = app.listen(process.env.PORT, () => {
   console.log(
-    `Server is running on http://localhost:${process.eventNames.PORT}`
+    `Server is running on http://localhost:${process.env.PORT}`
   );
 });
 
