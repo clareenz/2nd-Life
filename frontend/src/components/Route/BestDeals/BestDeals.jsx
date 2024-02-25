38ac33e0243d7e3a6e3054bbd6300229f70010cb
howtodoitpleasehelp <smacale@up.edu.ph>
2024-02-25 16:36:32 +0800

    5:16:01 / 7:37:10

---
+++ frontend/src/components/Route/BestDeals/BestDeals.jsx
@@ -0,0 +1,16 @@
+import React, { useState,useEffect } from 'react';
+import { productData } from '../../../static/data';
+
+const BestDeals = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const d = productData
+    }, [])
+
+  return (
+    <div></div>
+  )
+}
+
+export default BestDeals
