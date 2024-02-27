6d84b27f9f0a4b522fd918a62765e3f0b44cbc28
howtodoitpleasehelp <smacale@up.edu.ph>
2024-02-27 20:54:56 +0800

    6:36:50 / 7:37:10
    Done:
    - Featured Products
    - Events part of the homepage
    - Event Card
    - Countdown for the event card

---
+++ frontend/src/components/Events/CountDown.jsx
@@ -0,0 +1,56 @@
+/* The countdown part in the Events feature in homepage
+start time: 6:25:04 (first vid)
+ */
+
+import React, { useEffect, useState } from "react";
+
+const CountDown = () => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  function calculateTimeLeft() {
+    const difference = +new Date("2024-03-12") - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  }
+
+  const timerComponents = Object.keys(timeLeft).map((interval) => {
+    if (!timeLeft[interval]) {
+      return null;
+    }
+    
+    return (
+        <span className="text-[25px] text-[#475ad2]">
+            {timeLeft[interval]} {interval}{" "}
+        </span>
+    )
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <span className="text-[red] text-[25px]">Time's up!</span>
+      )}
+    </div>
+  );
+};
+
+export default CountDown;
