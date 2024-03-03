b9c95c0278758591dbba1166eb40eefdae594998
nikkamendoza <123077713+nikkamendoza@users.noreply.github.com>
2024-03-03 18:44:09 +0800

    improve format

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { navItems } from '../../static/data';
-import styles from '../../styles/styles';
+import React from "react";
+import { Link } from "react-router-dom";
+import { navItems } from "../../static/data";
+import styles from "../../styles/styles";
 
-const Navbar = ({active}) => {
+const Navbar = ({ active }) => {
   return (
     <div className={`block 800px:${styles.normalFlex}`}>
-         {
-            navItems && navItems.map((i,index) => (
-                <div className="flex">
-                    <Link to={i.url}
-                    className={`${active === index + 1 ? "text-[#fe8373]" : "text-black 800px:text-black"} pb-[30px] 800px:pb-0 font-[500] text-sm px-6 cursor-pointer }`}
-                    >
-                    {i.title}
-                    </Link>
-                </div>
-            ))
-         }
+      {navItems &&
+        navItems.map((i, index) => (
+          <div className="flex">
+            <Link
+              to={i.url}
+              className={`${
+                active === index + 1
+                  ? "text-[#fe8373]"
+                  : "text-black 800px:text-black"
+              } pb-[30px] 800px:pb-0 font-[500] text-sm px-6 cursor-pointer }`}
+            >
+              {i.title}
+            </Link>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
