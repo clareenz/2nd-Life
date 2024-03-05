54db9795dae9f5b94adae69ee894b6a2a27e78d3
howtodoitpleasehelp <smacale@up.edu.ph>
2024-03-05 23:04:32 +0800

    5:05:08 / 5:05:08 (2nd vid)
    Done:
    - Create Store page
    - Login Store page
    
    In Progress:
    - Shop Home Page

---
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -0,0 +1,12 @@
+/*  
+ * start time: 4:58:01 (2nd vid) 
+ */
+import React from 'react'
+
+const ShopHomePage = () => {
+  return (
+    <div>ShopHomePage</div>
+  )
+}
+
+export default ShopHomePage
