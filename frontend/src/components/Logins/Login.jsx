36e4f1bce21056a172501636e836eea95677225e
nikkamendoza <123077713+nikkamendoza@users.noreply.github.com>
2024-03-19 20:19:27 +0800

    design kay bored

--- frontend/src/components/Logins/Login.jsx
+++ frontend/src/components/Logins/Login.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { FaFacebook, FaGoogle } from "react-icons/fa"; // Import the appropriate icons
 import { Link, useNavigate } from "react-router-dom";
@@ -12,6 +12,33 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
+  const [emailClicked, setEmailClicked] = useState(false); // State to track email input click
+  const [passwordClicked, setPasswordClicked] = useState(false); // State to track password input click
+  const [isAutofilled, setIsAutofilled] = useState(false);
+
+  // Function to check if the input fields are autofilled
+  const checkAutofill = () => {
+    if (document.activeElement) {
+      const activeElement = document.activeElement;
+      setIsAutofilled(activeElement.value !== "");
+    }
+  };
+
+  const handleFocus = (field) => {
+    if (field === "email") {
+      setEmailClicked(true);
+    } else if (field === "password") {
+      setPasswordClicked(true);
+    }
+  };
+
+  const handleBlur = (field) => {
+    if (field === "email") {
+      setEmailClicked(false);
+    } else if (field === "password") {
+      setPasswordClicked(false);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -35,54 +62,60 @@ const Login = () => {
       });
   };
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      checkAutofill();
+    }, 200);
+
+    return () => clearTimeout(timeoutId);
+  }, [email, password]); // Listen for changes in email and password fields
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-gray-50 lg:flex-row sm:px-6 lg:px-8 login-div">
+    <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-gray-50 lg:flex-row login-div">
       {/* Left side with the image */}
-      <div className="lg:w-1/8 lg:pr-8">
+      <div className="lg:w-flex md:w-1/3 sm:w-1/3 w-1/4">
         <img
           src="/2ndLife_Logo.png"
           alt="2ndLife Logo"
-          className="mx-auto w-full h-auto lg:w-auto lg:h-auto"
         />
       </div>
 
       {/* Right side with the login form */}
-      <div className="lg:w-1/2">
+      <div className="lg:w-1/2 ">
         {/* Modified line */}
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-3xl font-bold text-center text-gray-900">
             Log in to 2ndLife
           </h2>
         </div>
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="mt-8 sm:mx-auto sm:w-flex sm:max-w-sm">
           <div className="px-4 py-8 bg-white shadow  sm:rounded-lg sm:px-10">
             <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
+              <div className="relative">
+                <input
+                  type="email"
+                  name="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  onFocus={() => handleFocus("email")}
+                  onBlur={() => handleBlur("email")}
+                  className="block w-full px-6 py-2 placeholder-gray-400 border bg-white border-gray-300 shadow-sm appearance-none rounded-3xl focus:outline-none focus:ring-[#006665] focus:border-[#006665] sm:text-sm relative"
+                />
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
+                  className={`absolute left-5 ${
+                    isAutofilled || emailClicked || email
+                      ? "transition transform -translate-y-[18px] bg-white h-3 top-2 text-xs px-1 text-[#006665] z-10"
+                      : "bottom-2.5 text-sm transition text-gray-500 text-center"
+                  }`}
                 >
                   Email address
                 </label>
-                <div className="mt-1">
-                  <input
-                    type="email"
-                    name="email"
-                    autoComplete="email"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="block w-full px-3 py-2 placeholder-gray-400 border-b border-gray-300 shadow-sm appearance-none square-md foucs:outline-none focus:ring-[#006665] focus:border-gray-500 sm:text-sm"
-                  />
-                </div>
               </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
+
+              <div className="relative">
                 <div className="relative mt-1">
                   <input
                     type={visible ? "text" : "password"}
@@ -91,23 +124,38 @@ const Login = () => {
                     required
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="block w-full px-3 py-2 placeholder-gray-400 border-b border-gray-300 shadow-sm appearance-none square-md foucs:outline-none focus:ring-[#006665] focus:border-gray-500 sm:text-sm"
+                    onFocus={() => handleFocus("password")}
+                    onBlur={() => handleBlur("password")}
+                    className="block w-full px-6 py-2 placeholder-gray-400 border bg-white border-gray-300 shadow-sm appearance-none rounded-3xl focus:outline-none focus:ring-[#006665] focus:border-[#006665] sm:text-sm relative"
                   />
                   {visible ? (
                     <AiOutlineEye
-                      className="absolute cursor-pointer right-2 top-2"
-                      size={20}
+                      className={`absolute cursor-pointer right-2 top-2/4 transform -translate-y-2/4 z-20`}
+                      size={17}
+                      style={{ color: passwordClicked ? "#006665" : "black" }}
                       onClick={() => setVisible(false)}
                     />
                   ) : (
                     <AiOutlineEyeInvisible
-                      className="absolute cursor-pointer right-2 top-2"
-                      size={20}
+                      className={`absolute cursor-pointer right-2 top-2/4 transform -translate-y-2/4 z-20`}
+                      size={17}
+                      style={{ color: passwordClicked ? "#006665" : "black" }}
                       onClick={() => setVisible(true)}
                     />
                   )}
                 </div>
+                <label
+                  htmlFor="password"
+                  className={`absolute left-5 ${
+                    isAutofilled || passwordClicked || password
+                      ? " transition transform -translate-y-[18px] bg-white h-3 top-2 text-xs px-1 text-[#006665] z-10"
+                      : "bottom-2.5 text-sm transition text-gray-500"
+                  }`}
+                >
+                  Password
+                </label>
               </div>
+
               <div className={`${styles.normalFlex} justify-between`}>
                 <div className={`${styles.normalFlex} focus:ring-[#006665]`}>
                   <input
@@ -126,16 +174,16 @@ const Login = () => {
                 <div className="text-sm">
                   <a
                     href="/forgot-password"
-                    className="font-medium text-006665 hover:text-fe8373"
+                    className="font-sm text-006665 hover:text-fe8373"
                   >
-                    Forgot your password?
+                    Forgot password?
                   </a>
                 </div>
               </div>
               <div>
                 <button
                   type="submit"
-                  className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-xl text-white bg-fe8373 hover:bg-006665"
+                  className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-3xl text-white bg-fe8373 hover:bg-006665"
                 >
                   Log in
                 </button>
@@ -150,7 +198,7 @@ const Login = () => {
               <div className="flex mt-4 space-x-4">
                 <button
                   type="button"
-                  className="group relative flex-1 h-[40px] flex items-center justify-center py-2 px-4 border border-gray-350 text-sm font-medium rounded-md text-black hover:bg-gray-100"
+                  className="group relative flex-1 h-[40px] flex items-center justify-center py-2 px-4 border border-gray-350 text-sm font-medium rounded-3xl text-black hover:bg-gray-100"
                 >
                   <FaFacebook className="mr-2" />
                   Facebook
@@ -158,7 +206,7 @@ const Login = () => {
 
                 <button
                   type="button"
-                  className="group relative flex-1 h-[40px] flex items-center justify-center py-2 px-4 border border-gray-350 text-sm font-medium rounded-md text-black hover:bg-gray-100"
+                  className="group relative flex-1 h-[40px] flex items-center justify-center py-2 px-4 border border-gray-350 text-sm font-medium rounded-3xl text-black hover:bg-gray-100"
                 >
                   <FaGoogle className="mr-2" />
                   Google
@@ -166,7 +214,7 @@ const Login = () => {
               </div>
 
               <div className={`${styles.normalFlex} w-full`}>
-                <h4>Not have any account?</h4>
+                <h4>Don't have an account?</h4>
                 <Link
                   to="/sign-up"
                   className="pl-2 text-006665 hover:text-fe8373"
