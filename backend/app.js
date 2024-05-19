ff56b8137d32ab139fdf6eb62b11ae429a2f142c
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-19 22:55:09 +0800

    updated back end

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://frontend-topaz-ten.vercel.app",
     credentials: true,
   })
 );
