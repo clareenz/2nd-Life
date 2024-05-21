89c342ebf93389dbe0a7240d9bb35525d4569203
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-21 20:33:05 +0800

    vercel

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server ="https://backend-1kv4ch9ki-clareenzs-projects.vercel.app/api/v2";
+export const server ="https://backend-topaz-ten.vercel.app/api/v2";
 
-export const backend_url = "https://zxbackend-1kv4ch9ki-clareenzs-projects.vercel.app/"
+export const backend_url = "https://backend-topaz-ten.vercel.app/"
