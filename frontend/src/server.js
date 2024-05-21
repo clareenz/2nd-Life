217c0e7697fa3c5f8b0784bcadeac6c2a5ecd35f
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-21 14:41:01 +0800

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server ="https://backend-steel-nu.vercel.app/api/v2";
+export const server ="https://backend-1kv4ch9ki-clareenzs-projects.vercel.app/api/v2";
 
-export const backend_url = "https://backend-steel-nu.vercel.app/"
+export const backend_url = "https://backend-1kv4ch9ki-clareenzs-projects.vercel.app/"
