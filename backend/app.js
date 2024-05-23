e398305f7e0d8baa6aef0c7cb86f1aee804579cc
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-23 14:45:40 +0800

    vercel

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,10 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ['https://frontend-topaz-ten.vercel.app',],
+  credentials: true
+}));
 app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
