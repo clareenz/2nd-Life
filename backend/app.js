77c14d2423acd094a326825f1a06549a4971bed7
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-26 12:44:56 +0800

    Deployment

--- backend/app.js
+++ backend/app.js
@@ -4,22 +4,26 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path =require("path");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://frontend-topaz-ten.vercel.app"],
     credentials: true,
   })
 );
-app.use("/", express.static("uploads"));
+app.use("/", express.static(path.join(__dirname,"./uploads")));
+app.use("/test", (req,res) => {
+  res.send("Hello world!");
+});
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 
