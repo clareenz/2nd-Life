4c1efb64251683140d7ea49fe88ad61ada6d4f4c
clareenz <148300455+clareenz@users.noreply.github.com>
2024-05-27 13:26:25 +0800

    Socket Added

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://frontend-topaz-ten.vercel.app"],
+    origin: [`https://frontend-topaz-ten.vercel.app`,'http://localhost:3000'],
     credentials: true,
   })
 );
